7d2b9bd1b7bfdbdf6ae94aa45e6f24096089dc8f
kevichi <kevin9716cui@gmail.com>
2021-11-01 17:48:21 +0200

    Update Solver.py
    Need to fix functions

--- Solver.py
+++ Solver.py
@@ -1,7 +1,8 @@
 class Solver:
 	size = 9
 	board = []
-	def __init__(self):
+	def __init__(self, board):
+		self.board = board
 		print("welcome to sudoku solver")
 	
 	def checkRow(row, value):
@@ -25,4 +26,52 @@ class Solver:
 					return false
 		return true
 
+	def isValid(row, column, value):
+		if checkRow(row,value) and checkColumn(column,value) and checkBlock(row,column,value):
+			return true
+
+	def getNextBlank(b):
+		for i in range(size):
+			for j in range(size):
+				if b[i][j] == 0:
+					return (i,j)
+
+	def solvePuzzle(self,b):
+		nextBlock = self.getNextBlank(b)
+		xPos = nextBlock[0]
+		yPos = nextBlock[1]
+
+		if not getNextBlank(b):
+			return true
+
+		for i in range(1,10):
+			if isValid(xPos,yPos,i):
+				b[xPos][yPos] = i
+
+		solvePuzzle(b)
+
+	
+
+
 			
+sudokuBoard = [
+	[7,8,0,4,0,0,1,2,0],
+	[6,0,0,0,7,5,0,0,9],
+	[0,0,0,6,0,1,0,7,8],
+	[0,0,7,0,4,0,2,6,0],
+	[0,0,1,0,5,0,9,3,0],
+	[9,0,4,0,6,0,0,0,5],
+	[0,7,0,3,0,0,0,1,2],
+	[1,2,0,0,0,7,4,0,0],
+	[0,4,9,2,0,6,0,0,7]
+]
+
+
+solver = Solver(sudokuBoard)
+for x in range(9):
+	print(sudokuBoard[x])
+
+solver.solvePuzzle(sudokuBoard)
+
+for x in range(9):
+	print(sudokuBoard[x])
